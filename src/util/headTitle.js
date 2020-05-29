b9ac768c2b3839cc9aadf4c92a7c3fd36f318f49
zhulifeng <519419249@qq.com>
2020-05-29 18:02:55 +0800

    add 选择地址切换列表

--- src/util/headTitle.js
+++ src/util/headTitle.js
@@ -1,25 +1,35 @@
 import { Header, Icon, Text } from 'react-native-elements';
-import { View } from 'react-native';
+import { TouchableHighlight, View } from 'react-native';
 import React, { Component } from 'react';
 
-export default class headTitle extends Component {
+export default class Head extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      title: ''
+    };
   }
+  onPress = () => {
+    this.props.navigation.navigate('cityChoose');
+  };
   render() {
     return (
-      <Header
-        centerComponent={
-          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-            <Icon iconStyle={{ marginRight: 15 }} name="ios-navigate" type="ionicon" size={18} color={'white'} />
-            <Text style={{ color: 'white' }}>即将上映!</Text>
-          </View>
-        }
-        containerStyle={{
-          backgroundColor: 'black',
-          justifyContent: 'center'
-        }}
-      />
+      <View>
+        <Header
+          centerComponent={
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <TouchableHighlight onPress={this.onPress}>
+                <Icon iconStyle={{ marginRight: 15 }} name="sc-telegram" type="evilicon" size={18} color={'white'} />
+              </TouchableHighlight>
+              <Text style={{ color: 'white' }}>{this.props.title}</Text>
+            </View>
+          }
+          containerStyle={{
+            backgroundColor: 'black',
+            justifyContent: 'center'
+          }}
+        />
+      </View>
     );
   }
 }
