efc5018c60190fc528e54f52a218126b1f5aa108
zlf <519419249@qq.com>
2021-03-03 17:17:00 +0800

    icon 字体 依赖包版本 修改 改版网易云

--- src/views/Index.js
+++ src/views/Index.js
@@ -12,27 +12,49 @@ export default class Index extends Component {
     };
   }
 
-  renderCard(item) {
+  renderCard(item, index) {
     return (
-      <Card key={item.name}>
-        <Text>{item.name}</Text>
+      <Card key={index}>
+        <View style={{ flexDirection: 'column', alignItems: 'center', borderRadius: 20 }}>
+          <Image source={{ uri: item.picUrl }} style={{ width: 220, height: 220, marginBottom: 20, borderRadius: 20 }} />
+          <Text style={{ marginBottom: 20 }} key={item.pic}>
+            {item.name}
+          </Text>
+          <Icon
+            name="play-circle"
+            type="feather"
+            iconStyle={{ height: 25, width: 25, marginLeft: 10, marginRight: 10 }}
+            size={25}
+            color="#0066CC"
+            onPress={() => this.playVideo(item.id)}
+          />
+        </View>
       </Card>
     );
   }
   UNSAFE_componentWillMount() {
     util.get('https://netease-cloud-music-api-azure.vercel.app/recommend/songs', (res) => {
-      this.setState({
-        list: res.data.dailySongs
+      let arrList = res.data.dailySongs;
+      arrList.map((item, index) => {
+        arrList[index].picUrl = item.al.picUrl;
       });
+      this.setState({
+        list: arrList
+      });
+    });
+  }
+  //播放音乐
+  playVideo(id) {
+    util.get('https://netease-cloud-music-api-azure.vercel.app/song/url?id=' + id, (res) => {
       console.log('====================================');
-      console.log(res.data.dailySongs);
+      console.log(res.data.url);
       console.log('====================================');
     });
   }
   render() {
     return (
-      <View>
-        <FlatList extraData={this.state.list} windowSize={10} keyExtractor={(item) => item.name} data={this.state.list} renderItem={({ item }) => this.renderCard(item)} />
+      <View style={{ marginBottom: 10 }}>
+        <FlatList windowSize={10} keyExtractor={(item) => item.name} data={this.state.list} renderItem={({ item, index }) => this.renderCard(item, index)} />
       </View>
     );
   }
